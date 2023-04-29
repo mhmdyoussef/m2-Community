a15697474ce41c9c708f86ca090559948b778208
Mohamed Youssef <egsyntax@gmail.com>
2023-04-29 04:29:08 +0300

    registering new module with database schema

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * MY-Dev
+ * @category    MY-Dev
+ * @copyright   Copyright (c) 2023. MY-Dev (https://www.my-dev.pro/)
+ * @license     http://www.my-dev.pro/license-agreement.html
+ * @Author: Mohamed Youssef<mydevpro1@gmail.com>
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MyDev_Community', __DIR__);
